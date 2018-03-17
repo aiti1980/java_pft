13e7130cf3cda8d479da93c5a5b3a70952fbae45
aiti1980 <sidorenko-p@inbox.ru>
2018-03-18 02:22:05 +0300

    ДЗ2.Ответ

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -3,26 +3,21 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgramm {
 
  public static void main(String[] args) {
-  hello("world");
-  hello("user");
-  hello("Arena");
-  double l = 5;
-  System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-  double a = 4;
-  double b = 6;
-  System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  Point p1 = new Point();
+  Point p2 = new Point();
+  p1.a=12.0;
+  p1.b=14.0;
+  p2.c=18.0;
+  p2.d=18.0;
+
+  System.out.println("Distance from point A(" + p1.a +"," + p1.b +") "+ "to B(" +  + p2.c +"," + p2.d +")" + " = " + distance(p1,p2));
+
+ }
+ public static double distance (Point p1, Point p2)
+
+ {
+  return Math.sqrt(Math.pow(p2.c - p1.a,2) + Math.pow(p2.d-p1.b,2));
+
  }
 
- public static void hello(String somebody) {
-
-  System.out.println("Hello, " + somebody + "!");
- }
-
- public static double area(double len) {
-  return len * len;
- }
-
- public static double area(double a, double b) {
-  return a * b;
- }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public double a;
+  public double b;
+  public double c;
+  public double d;
+  //public Point (double a, double b, double c, double d){
+   // this.a=a;
+   //this.b=b;
+   //this.b=c;
+  //this.b=d;
+ // }
+
+}
