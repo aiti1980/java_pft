6d3444333026b324bfa7fb40aee5ebecbcbd7857
aiti1980 <sidorenko-p@inbox.ru>
2018-03-18 23:10:07 +0300

    ДЗ-2 полностью

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -3,17 +3,9 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgramm {
 
  public static void main(String[] args) {
-  Point p1 = new Point(10.0,12.0);
-  Point p2 = new Point(13.0,16.0);
-
-  System.out.println("Distance from point A(" + p1.x +"," + p1.y +") "+ "to B(" +  + p2.x +"," + p2.y +")" + " = " + distance(p1,p2));
-
- }
- public static double distance (Point p1, Point p2)
-
- {
-  return Math.sqrt(Math.pow(p2.x - p1.x,2) + Math.pow(p2.y-p1.y,2));
-
+  Point p1 = new Point(10.0, 12.0);
+  Point p2 = new Point(13.0, 16.0);
+  System.out.println("Distance from point A(" + p1.x + "," + p1.y + ") " + "to B(" + p2.x + "," + p2.y + ")" + " = " + p2.distance(p1));
  }
 
 }
