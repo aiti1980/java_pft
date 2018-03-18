a820f98fd4f1b7f5f5fd14d23fd7a7179028c1f7
aiti1980 <sidorenko-p@inbox.ru>
2018-03-18 13:00:36 +0300

    ДЗ-2. Пока без пункта 4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -3,20 +3,16 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgramm {
 
  public static void main(String[] args) {
-  Point p1 = new Point();
-  Point p2 = new Point();
-  p1.a=12.0;
-  p1.b=14.0;
-  p2.c=18.0;
-  p2.d=18.0;
+  Point p1 = new Point(10.0,12.0);
+  Point p2 = new Point(13.0,16.0);
 
-  System.out.println("Distance from point A(" + p1.a +"," + p1.b +") "+ "to B(" +  + p2.c +"," + p2.d +")" + " = " + distance(p1,p2));
+  System.out.println("Distance from point A(" + p1.x +"," + p1.y +") "+ "to B(" +  + p2.x +"," + p2.y +")" + " = " + distance(p1,p2));
 
  }
  public static double distance (Point p1, Point p2)
 
  {
-  return Math.sqrt(Math.pow(p2.c - p1.a,2) + Math.pow(p2.d-p1.b,2));
+  return Math.sqrt(Math.pow(p2.x - p1.x,2) + Math.pow(p2.y-p1.y,2));
 
  }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,15 +1,19 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  public double a;
-  public double b;
-  public double c;
-  public double d;
-  //public Point (double a, double b, double c, double d){
-   // this.a=a;
-   //this.b=b;
-   //this.b=c;
-  //this.b=d;
- // }
+  public double x;
+  public double y;
+
+public Point (double x, double y) {
+  this.x = x;
+  this.y = y;
+}
+//}
+// public static double distance (Point p1, Point p2)
+
+ // {
+ //   return Math.sqrt(Math.pow(p2.x - p1.x,2) + Math.pow(p2.y-p1.y,2));
+//
+//  }
 
 }
