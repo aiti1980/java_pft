37aaac1784febd1c858e748d19c471e7a01a3c47
aiti1980 <sidorenko-p@inbox.ru>
2018-03-20 03:26:23 +0300

    ДЗ-4. Создание контакта

---
+++ sandbox/addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String lastName;
+  private final String mobile;
+  private final String email;
+
+  public ContactData(String name, String lastName, String mobile, String email) {
+    this.name = name;
+    this.lastName = lastName;
+    this.mobile = mobile;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
