1b5bc450cbfe2620ad5ecd300dc9eda74b453c5b
aiti1980 <sidorenko-p@inbox.ru>
2018-03-22 03:34:59 +0300

    DZ-6.Not full

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appGroup/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appGroup/GroupHelper.java
@@ -9,35 +9,43 @@ import ru.stqa.pft.addressbook.HelperBase.HelperBase;
  * @author p.sidorenko.
  * @since 21.03.2018.
  */
-public class GroupHelper extends HelperBase{
+public class GroupHelper extends HelperBase {
 
-    public GroupHelper(FirefoxDriver wd) {
-        super (wd);
-    }
+  public GroupHelper(FirefoxDriver wd) {
+    super(wd);
+  }
 
-    public void returnToGroupPage() {
-        click(By.linkText("group page"));
-    }
+  public void returnToGroupPage() {
+    click(By.linkText("group page"));
+  }
 
-    public void submitGroupCreation() {
-        click(By.name("submit"));
-    }
+  public void submitGroupCreation() {
+    click(By.name("submit"));
+  }
 
-    public void fillgroupForm(SessionHelperGroupAndContact.GroupData groupData) {
-        type("group_name", groupData.getName());
-        type("group_header", groupData.getHeader());
-        type("group_footer", groupData.getFooter());
-    }
+  public void fillgroupForm(SessionHelperGroupAndContact.GroupData groupData) {
+    type("group_name", groupData.getName());
+    type("group_header", groupData.getHeader());
+    type("group_footer", groupData.getFooter());
+  }
 
-    public void initGroupCreation() {
-        click(By.name("new"));
-    }
+  public void initGroupCreation() {
+    click(By.name("new"));
+  }
 
-    public void deleteSelectedGroups() {
-        click(By.name("delete"));
-    }
+  public void deleteSelectedGroups() {
+    click(By.name("delete"));
+  }
 
-    public void selectGroup() {
-        click(By.name("selected[]"));
-    }
+  public void selectGroup() {
+    click(By.name("selected[]"));
+  }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
+  }
 }
